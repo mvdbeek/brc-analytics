e16ac8d96beb755ff4c7156e0e0364c554ba69f0
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-09-13 14:49:27 +1000

    feat: the return of "your questions answered!" (#56)

--- app/components/Home/components/Section/components/SectionHelp/sectionHelp.tsx
+++ app/components/Home/components/Section/components/SectionHelp/sectionHelp.tsx
@@ -1,10 +1,28 @@
+import { Link as DXLink } from "@databiosphere/findable-ui/lib/components/Links/components/Link/link";
 import { Questions } from "./components/Questions/questions";
-import { Section, SectionLayout } from "./sectionHelp.styles";
+import {
+  Headline,
+  Section,
+  SectionLayout,
+  StyledSectionSubTitle,
+  StyledSectionTitle,
+} from "./sectionHelp.styles";
 
 export const SectionHelp = (): JSX.Element => {
   return (
     <Section>
       <SectionLayout>
+        <Headline>
+          <StyledSectionTitle>Your questions, answered</StyledSectionTitle>
+          <StyledSectionSubTitle>
+            If you need further assistance, feel free to reach out to us at{" "}
+            <DXLink
+              label="help@brc-analytics.org"
+              url="mailto:help@brc-analytics.org"
+            />
+            . We&#39;re here to help!
+          </StyledSectionSubTitle>
+        </Headline>
         <Questions />
       </SectionLayout>
     </Section>
