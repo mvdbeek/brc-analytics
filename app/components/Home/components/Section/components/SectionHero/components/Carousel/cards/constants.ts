90864c25981509a555a9a651e79a692ce908cfad
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-10-08 10:22:04 +1000

    feat: comment out current homepage card about the webinar (#124)

--- app/components/Home/components/Section/components/SectionHero/components/Carousel/cards/constants.ts
+++ app/components/Home/components/Section/components/SectionHero/components/Carousel/cards/constants.ts
@@ -2,9 +2,9 @@ import { CardProps } from "@databiosphere/findable-ui/lib/components/common/Card
 import * as MDX from "../content";
 
 export const CAROUSEL_CARDS: Pick<CardProps, "text">[] = [
-  {
-    text: MDX.Webinars({}),
-  },
+  // {
+  //   text: MDX.Webinars({}),
+  // },
   {
     text: MDX.ShareUsageAndJoinAdvisoryPanel({}),
   },
