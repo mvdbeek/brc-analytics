a450ea515c5ab03ff5874a35914e60c1ce5d8e05
Hunter Craft <118154470+hunterckx@users.noreply.github.com>
2025-03-10 18:11:28 -0700

    ci: fix linkml checks so that they exit with failure if the check fails for any file (#341) (#376)

--- catalog/schema/scripts/test-gen-python.sh
+++ catalog/schema/scripts/test-gen-python.sh
@@ -2,7 +2,17 @@
 
 source ./catalog/schema/scripts/source-file-schema-names.sh
 
+validation_failed=false
+
 for name in schema ${SOURCE_FILE_SCHEMA_NAMES[@]}
 do
   gen-python "./catalog/schema/$name.yaml" > /dev/null
+  if [ $? -ne 0 ]; then
+    validation_failed=true
+  fi
 done
+
+if [ "$validation_failed" = true ]; then
+  echo "Generation failed for one or more schemas."
+  exit 1
+fi
--- catalog/schema/scripts/validate-catalog.sh
+++ catalog/schema/scripts/validate-catalog.sh
@@ -2,9 +2,19 @@
 
 source ./catalog/schema/scripts/source-file-schema-names.sh
 
+validation_failed=false
+
 for name in ${SOURCE_FILE_SCHEMA_NAMES[@]}
 do
   echo "$name:"
   linkml-validate -s "./catalog/schema/$name.yaml" "./catalog/source/$name.yml"
+  if [ $? -ne 0 ]; then
+    validation_failed=true
+  fi
   echo ""
 done
+
+if [ "$validation_failed" = true ]; then
+  echo "Validation failed for one or more schemas."
+  exit 1
+fi
